2b420bb80d1386c1a0975b0cc8ad0c4c998db7ef
arif rismawan <areffrezma@gmail.com>
2018-07-24 21:53:51 +0700

    [rezma] first commit

---
+++ src/Ayam.java
@@ -0,0 +1,3 @@
+public class Ayam {
+    String kaki;
+}
---
+++ src/Hewan.java
@@ -0,0 +1,8 @@
+public class Hewan {
+    String jumlahKaki;
+    String alatNafas;
+
+    public static void main (String args []){
+        Kaki kaki = new Kaki();
+    }
+}
